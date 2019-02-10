495245dae354ddce8f5da5843b169401dc7411a6
AlexKu <kudashof@gmail.com>
2019-02-10 21:24:34 +0300

    Шаблонизация v1

---
+++ data.php
@@ -0,0 +1,44 @@
+<?php
+
+//массив категорий
+$category_list = ['Входящие', 'Учеба', 'Работа', 'Домашние дела', 'Авто'];
+
+//список задач
+$tasks_list = [
+    [
+        'title' => 'Собеседование в IT компании',
+        'date' => '01.12.2019',
+        'category' => $category_list[2],
+        'complete' => false
+    ],
+    [
+        'title' => 'Выполнить тестовое задание',
+        'date' => '25.12.2019',
+        'category' => $category_list[2],
+        'complete' => false
+    ],
+    [
+        'title' => 'Сделать задание первого раздела',
+        'date' => '21.12.2019',
+        'category' => $category_list[1],
+        'complete' => true
+    ],
+    [
+        'title' => 'Встреча с другом',
+        'date' => '22.12.2019',
+        'category' => $category_list[0],
+        'complete' => false
+    ],
+    [
+        'title' => 'Купить корм для кота',
+        'date' => 'Нет',
+        'category' => $category_list[3],
+        'complete' => false
+    ],
+    [
+        'title' => 'Заказать пиццу',
+        'date' => 'Нет',
+        'category' => $category_list[3],
+        'complete' => false
+    ]
+];
---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!is_readable($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require $name;
+
+    $result = ob_get_clean();
+
+    return $result;
+}
+
+// показывать или нет выполненные задачи
+$show_complete_tasks = rand(0, 1);
+
+function count_category($tasks_list, $project_name){
+    $count=0;
+    foreach ($tasks_list as $item) {
+        if ($item['category']==$project_name){
+            $count++;
+        }
+    }
+    return $count;
+}
