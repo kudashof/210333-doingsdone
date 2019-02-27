181d7efa79ff20de362d1fd9c35611c709983f0c
AlexKu <kudashof@gmail.com>
2019-02-27 20:32:24 +0300

    Формы и протокол HTTP t1 v3 правки

--- index.php
+++ index.php
@@ -14,11 +14,25 @@ if ($con == false) {
     $projects = db_fetch_data($link, $sql_project, $id);
     $tasks = db_fetch_data($link, $sql_task, $id);
 }
+//
+//$sql = 'SELECT tasks.name, tasks.date_deadline, tasks.status FROM tasks WHERE tasks.user_id = ? AND tasks.project_id = ?';
+////$sql = 'SELECT project_id FROM tasks WHERE project_id = ? LIMIT 1';
+//if (isset($_GET['projectID'])) {
+//    $count_id = db_fetch_data($link, $sql, $_GET['projectID']);
+//    if ($count_id == false) {
+//        http_response_code(404);
+//    }
+//}
 
-$sql = 'SELECT project_id FROM tasks WHERE project_id = ? LIMIT 1';
 if (isset($_GET["projectID"])) {
-    $count_id = db_fetch_data($link, $sql, $_GET["projectID"]);
-    if ($count_id==false) {
+    $is_project = 'SELECT id FROM projects WHERE id = ? LIMIT 1';
+    if ($is_project) {
+        // Если есть, получить задачи с отбором по id проекта, для отбора использовать выше указанный параметр из $_GET
+        $sql = 'SELECT project_id FROM tasks WHERE project_id = ? LIMIT 1';
+        // Все задачи по проекту положить в переменную $tasks
+        $tasks = db_fetch_data($link, $sql, $_GET['projectID']);
+    } else {
+        // Если такого проекта нет, то 404
         http_response_code(404);
     }
 }
@@ -35,8 +49,6 @@ $layout_content = include_template('layout.php', [
     'user_name' => 'Имя пользователя'
 ]);
 print($layout_content);
-
-
 
 
 ?>
